1a4ea5c5410adb37719e761c9b319bbd24993313
krupalibusa <64123898+krupalibusa@users.noreply.github.com>
2024-07-25 10:47:57 +0530

    Add files via upload

---
+++ chapter5/if.py
@@ -0,0 +1,7 @@
+result = float(input("Enter your percentage"))
+if(result >= 70.00):
+    print("Ececellent and you have passed")
+if(result <70 and result >=50):
+    print("good you have passed")
+if(result <50):
+    print("Better luck for next time")
